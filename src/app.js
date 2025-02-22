5ef455f5b9452e188ef4303e593fc8e63e0f40a2
Goosa2025 <gus21002@byui.edu>
2025-02-22 00:47:51 -0700

    cors troubleshooting

--- src/app.js
+++ src/app.js
@@ -4,10 +4,19 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 const r = require('./routes/routes')
 
+const allowedOrigins = ['http://192.168.88.1:5500', 'https://airandember.com']
 
 // Middleware to parse JSON bodies
 app.use(express.json())
-    .use(cors())
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }))
     .use('/', r);
 
 // Simple route for testing
