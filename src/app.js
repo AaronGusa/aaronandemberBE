f5a541a45eec1796049dcc27030a1479e3bbefdd
AaronGusa <gus21002@byui.edu>
2025-02-23 18:41:47 -0700

    cors allowed origins

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 const r = require('./routes/routes')
 
-const allowedOrigins = ['http://192.168.88.1:5500', 'https://airandember.com']
+const allowedOrigins = ['http://192.168.88.1:5500','http://192.168.86.42:5500/', 'https://airandember.com']
 
 // Middleware to parse JSON bodies
 app.use(express.json())
