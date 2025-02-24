63f86744919077c99d1f34a9139766c7028125c7
AaronGusa <gus21002@byui.edu>
2025-02-24 16:03:13 -0700

    console. logs to track runner

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -9,12 +9,14 @@ async function proveFirebaseToken(req, res, next) {
       return res.status(401).send('Unauthorized');
     }
     try {
+        console.log('Entered Try')
       const decodedToken = await admin.auth().verifyIdToken(token);
       console.log(decodedToken);
       req.isAuthenticated = true;
       req.user = decodedToken;
       next();
     } catch (error) {
+        console.log('Entered Error')
       req.isAuthenticated = false;
       res.status(401).send('Invalid token');
     }
