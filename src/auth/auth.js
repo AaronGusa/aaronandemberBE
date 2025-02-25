27a69a7ff051c914c20da1fbcbaacbda8115dcfd
AaronGusa <gus21002@byui.edu>
2025-02-24 23:19:10 -0700

    coonsole log cleanup

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -21,20 +21,20 @@ admin.initializeApp({
 // Middleware to verify Firebase ID token
 async function proveFirebaseToken(req, res, next) {
     const token = req.headers.authorization?.split(' ')[1];
-    console.log(token);
+    // console.log(token);
     if (!token) {
-        console.log('entered not token?')
+        // console.log('entered not token?')
       return res.status(401).send('Unauthorized');
     }
     try {
-      console.log('Entered Try')
+    //   console.log('Entered Try')
       const decodedToken = await admin.auth().verifyIdToken(token);
-      console.log(decodedToken);
+    //   console.log(decodedToken);
       req.isAuthenticated = true;
       req.user = decodedToken;
       next();
     } catch (error) {
-      console.log('Entered Error')
+    //   console.log('Entered Error')
       req.isAuthenticated = false;
       res.status(401).send('Invalid token');
     }
