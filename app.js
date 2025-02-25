765bae21d9edbf0c25a277f749f103c5393cdc4f
AaronGusa <gus21002@byui.edu>
2025-02-24 17:03:50 -0700

    firebase setup

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
-const admin = require('firebase-admin');
 const r = require('./src/routes/routes')
 
 const allowedOrigins = ['http://192.168.88.1:5500','http://192.168.86.42:5500', 'https://airandember.com']
-
-admin.initializeApp({
-    credential: admin.credential.applicationDefault()
-});
 
 
 // Middleware to parse JSON bodies
@@ -26,11 +21,6 @@ app.use(express.json())
         credentials: true
     }))
     .use('/', r);
-
-// Simple route for testing
-// app.get('/', (req, res) => {
-//     res.send('Hello, Render!');
-// });
 
 // Start the server
 app.listen(port, () => {
