5e9b80d089da6c2f93363b3d5d17d67a40c8ae73
AaronGusa <gus21002@byui.edu>
2025-02-24 21:42:07 -0700

    attempt to receive stripe api endpoints

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,11 @@
 const indx_r = require('express').Router();
 const pft = require('../auth/auth');
 
-indx_r.use('/service', pft.proveFirebaseToken, require('./stripeR'));
+//indx_r.use('/service', pft.proveFirebaseToken, require('./stripeR'));
+indx_r.use('/service', proveFirebaseToken, (req, res, next) => {
+    console.log('Middleware passed, accessing service route...');
+    next();
+}, require('./stripeR'));
 
 // Default route
 indx_r.use('/', (req, res) => {
