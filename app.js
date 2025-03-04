312368f3d7b71121e659d6e3d1396ee125a9568e
Goosa2025 <gus21002@byui.edu>
2025-03-03 23:48:20 -0700

    cors error?

--- app.js
+++ app.js
@@ -2,25 +2,31 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
-const r = require('./src/routes/routes')
+const routes = require('./src/routes/routes');
 
-const allowedOrigins = ['http://192.168.88.1:5500','http://192.168.86.42:5500', 'https://airandember.com']
-
+const allowedOrigins = [
+    'http://192.168.88.1:5500',
+    'http://192.168.86.42:5500', 
+    'https://airandember.com'
+];
 
 // Middleware to parse JSON bodies
-app.use(express.json())
-//CORS config
-    .use(cors({
-        origin: function (origin, callback) {
+app.use(express.json());
+
+// CORS config
+app.use(cors({
+    origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
-        },
-        credentials: true
-    }))
-    .use('/', r);
+    },
+    credentials: true
+}));
+
+// Use routes
+app.use('/', routes);
 
 // Start the server
 app.listen(port, () => {
